ca60312c76b077afdf88e29189f32100c2f9aa3a
Ke Li <likekeustc@hotmail.com>
2015-06-03 17:49:31 -0400

    Create SymbolParams.cs

---
+++ SymbolParams.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZebraBarPrintApp
+{
+    public class SymbolParams
+    {
+        public int fromLeft = new int();
+        public int fromTop = new int();
+        public int height = new int();
+        public int width = new int();
+        public int BYvalue = new int();
+    }
+}
